475ea5b51088a1df89e5cb1346a46771537da389
AndryMorgan <rafanoharanamorgan4@gmail.com>
2026-05-28 08:58:49 +0300

    add the specific fonctionality create

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { EmployeeList } from "./employees/EmployeeList";
 import { EmployeeEdit } from "./employees/EmployeeEdit";
+import { EmployeeCreate } from "./employees/EmployeeCreate";
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="employees" list={EmployeeList} edit={EmployeeEdit} />
+    <Resource name="employees" list={EmployeeList} edit={EmployeeEdit} create={EmployeeCreate} />
   </Admin>
 );
 
---
+++ src/employees/EmployeeCreate.tsx
@@ -0,0 +1,43 @@
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  NumberInput,
+  BooleanInput,
+  SelectInput,
+  required,
+  minValue,
+  email,
+} from 'react-admin';
+
+
+export const EmployeeCreate = () => (
+  <Create>
+    <SimpleForm>
+      <TextInput source="firstname" label="Prénom" validate={required()} />
+      <TextInput source="lastname" label="Nom" validate={required()} />
+      <TextInput
+        source="email"
+        label="Email"
+        validate={[required(), email()]}
+      />
+      <SelectInput
+        source="department"
+        label="Département"
+        validate={required()}
+        choices={[
+          { id: 'Informatique', name: 'Informatique' },
+          { id: 'Marketing', name: 'Marketing' },
+          { id: 'RH', name: 'RH' },
+          { id: 'Finance', name: 'Finance' },
+        ]}
+      />
+      <NumberInput
+        source="salary"
+        label="Salaire"
+        validate={[required(), minValue(1500, 'Minimum 1500')]}
+      />
+      <BooleanInput source="active" label="Actif" defaultValue={true} />
+    </SimpleForm>
+  </Create>
+);
